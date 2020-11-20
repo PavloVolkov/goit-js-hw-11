633f45dc8239643dacef598c398e0000c0d548ee
PavloVolkov <volkovpavelm3@gmail.com>
2020-11-20 20:39:58 +0200

    hw-11

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './styles.css';
+import './timer.js';
+import './color.js';
+import './promise';
+import './promise2';
+import './promise3'
+
---
+++ src/promise.js
@@ -0,0 +1,19 @@
+const delay = ms => {
+    const promise = new Promise((resolve, reject)=>{
+        if(ms){
+            setTimeout(()=>{
+                resolve(ms);
+             }, ms)
+        }
+
+    })
+    return promise
+  };
+  
+  const logger = time => console.log(`Resolved after ${time}ms`);
+  
+  delay(2000).then(logger); // Resolved after 2000ms
+  delay(1000).then(logger); // Resolved after 1000ms
+  delay(1500).then(logger); // Resolved after 1500ms
+
+ 
